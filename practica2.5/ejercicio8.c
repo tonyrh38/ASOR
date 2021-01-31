2240f4d406261e6cb11f2904ffd9491a71cf0684
Antonio Rodríguez Hurtado <antrodrigh@gmail.com>
2021-01-31 18:30:53 +0100

    Ejercicio 8

---
+++ practica2.5/ejercicio8.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <time.h>
+
+int main(int argc, char *argv[]){
+	
+	if (argc != 3) return -1;
+
+	struct addrinfo hints;
+	struct addrinfo *info;
+
+	hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM; 
+  hints.ai_flags = AI_PASSIVE;    
+  hints.ai_protocol = 0;          
+  hints.ai_canonname = NULL;
+  hints.ai_addr = NULL;
+  hints.ai_next = NULL;
+	
+	getaddrinfo(argv[1], argv[2], &hints, &info);
+	int sd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);	
+	bind(sd, info->ai_addr, info->ai_addrlen);
+	listen(sd, 5);
+
+	struct sockaddr_storage sa;
+	socklen_t sa_len = sizeof(sa);
+	char host[NI_MAXHOST];
+	char socket[NI_MAXSERV];
+	char buf[128];
+	
+	while(1){
+		int cli_sd = accept(sd, (struct sockaddr *)&sa, &sa_len);
+		int pid = fork();
+		if(pid == 0){
+			getnameinfo((struct sockaddr *)&sa, sa_len, host, NI_MAXHOST, socket, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+			int n;
+			while(n = recv(cli_sd, buf, 128, 0)){
+				buf[n] = '\0';
+				printf("[%d] Conexion desde %s:%s> %s", getpid(), host, socket, buf);
+				send(cli_sd, buf, n, 0);
+			}
+			printf("[%d] Conexion terminada\n", getpid());
+			close(cli_sd);
+			return 0;
+		}
+		else close(cli_sd);
+	}
+
+	return 0;
+}
